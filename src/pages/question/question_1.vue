6d84aa472947aa8cc3d00cbf144c3097af7252cd
可乐加冰 <846991641@qq.com>
2023-06-08 21:47:19 +0800

    question

--- src/pages/question/question_1.vue
+++ src/pages/question/question_1.vue
@@ -2,31 +2,64 @@
   <div class="question-box">
     <h3>问题1</h3>
     <p>如下级联所示: 树状结构JSON</p>
-    <el-cascader
-      v-model="value"
-      :options="data"
-      :show-all-levels="false"
-    ></el-cascader>
+    <el-cascader v-model="value" :options="data" :show-all-levels="false"></el-cascader>
     <p>要求: 实现叶子节点的下拉项</p>
-    <el-select v-model="leafValue"></el-select>
+    <el-select v-model="leafValue">
+      <el-option v-for="item in flatChildren" :key="item.tagId" :value="item.tagId" :label="item.label"></el-option>
+    </el-select>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
 import { data } from '@/mock/mock'
 export default {
-  data () {
+  data() {
     return {
       data,
       value: null,
       leafValue: null
+    }
+  },
+  computed: {
+    //  只取第三层children
+    flatChildren() {
+      const selectList = []
+      this.data &&
+        this.data.forEach(item => {
+          item.children &&
+            item.children.forEach(cItem => {
+              cItem.children &&
+                cItem.children.forEach(sItem => {
+                  selectList.push(sItem)
+                })
+            })
+        })
+      return selectList
+    },
+    //  取全部
+    flatAllData() {
+      if (!this.data || !Array.isArray(this.data)) return []
+      return this.flatFun(this.data)
+    }
+  },
+  methods: {
+    flatFun(list) {
+      const selectList = []
+      list.forEach(item => {
+        selectList.push(item)
+        if (item.children && item.children.length > 0) {
+          selectList.push(...this.flatFun(item.children))
+        }
+      })
+      return selectList
     }
   }
 }
 </script>
 
 <style scoped>
-.question-box{
+.question-box {
   display: flex;
   flex-direction: column;
   align-items: start;
--- src/pages/question/question_2.vue
+++ src/pages/question/question_2.vue
@@ -2,18 +2,22 @@
   <div class="question-box">
     <h3>问题2</h3>
     <p>使用h5c3或其他方式，实现下图中的五角星</p>
-    <img src="../../assets/q2.png" alt="" style="width: 100px;aspect-ratio: 1;">
-    <div class="pentagram"></div>
+    <img src="../../assets/q2.png" alt="" style="width: 100px; aspect-ratio: 1" />
+    <div class="pentagram">
+      <div class="pentagram-left"></div>
+      <div class="pentagram-mid"></div>
+      <div class="pentagram-right"></div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-}
+/* eslint-disable */
+export default {}
 </script>
 
 <style scoped>
-.question-box{
+.question-box {
   display: flex;
   flex-direction: column;
   align-items: start;
@@ -21,6 +25,30 @@ export default {
   gap: 8px;
 }
 .pentagram {
-
+}
+.pentagram-left,
+.pentagram-mid,
+.pentagram-right {
+  width: 0px;
+  height: 0px;
+  border-left: 40px solid #000000;
+  border-right: 40px solid transparent;
+  border-top: 40px solid transparent;
+  border-bottom: 40px solid transparent;
+}
+.pentagram-left {
+  transform: rotateZ(24deg);
+}
+.pentagram-mid {
+  transform: rotateZ(90deg);
+  position: relative;
+  top: -72px;
+  left: -20px;
+}
+.pentagram-right {
+  transform: rotateZ(156deg);
+  position: relative;
+  top: -160px;
+  left: -40px;
 }
 </style>
